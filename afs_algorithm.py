c5727ac7b8a6166d23d3071cb5cf392240c011c3
vasilistsirm <it22111@hua.gr>
2023-06-24 00:42:00 +0300

    Commit

--- afs_algorithm.py
+++ afs_algorithm.py
@@ -3,6 +3,8 @@ import numpy as np
 # Parameters
 num_fish = 100
 num_iterations = 100
+# if it doesn't work for that many iterations/fish u can change the number to a lower one, but the results
+# will not be as optimal
 visual_range = 0.1
 step_size = 0.1
 
@@ -15,6 +17,7 @@ user_money_rates = np.delete(user_money_rates, (0), axis=0)
 
 album_price = np.random.randint(50, size=100) + 1
 
+
 # Define fitness function (unchanged)
 def calculate_individual_fitness(fish, i):
     fish = fish.reshape(user_money_rates.shape[0], 3)
@@ -24,16 +27,19 @@ def calculate_individual_fitness(fish, i):
         total_score -= total_score * (total_price - users_money[i]) / total_price
     return total_score
 
+
 # Create initial fish population (unchanged)
 def initialize_fish_population():
     initial_fish_population = []
     for _ in range(num_fish):
         fish = np.zeros(user_money_rates.shape[0] * 3)
         fish[:user_money_rates.shape[0]] = user_money_rates[:, 0]
-        fish[user_money_rates.shape[0]:2 * user_money_rates.shape[0]] = np.random.choice(album_price, size=user_money_rates.shape[0])
+        fish[user_money_rates.shape[0]:2 * user_money_rates.shape[0]] = np.random.choice(album_price,
+                                                                                         size=user_money_rates.shape[0])
         fish[2 * user_money_rates.shape[0]:] = user_money_rates[:, 1]
         initial_fish_population.append(fish)
     return initial_fish_population
+
 
 # Update fish position and behavior (unchanged)
 def update_fish(fish):
@@ -48,6 +54,7 @@ def update_fish(fish):
         new_fish_population.append(current_fish)
     return new_fish_population
 
+
 # Get neighboring fish within the visual range (unchanged)
 def get_neighbors(fish, population):
     neighbors = []
@@ -56,11 +63,13 @@ def get_neighbors(fish, population):
             neighbors.append(other_fish)
     return neighbors
 
+
 # Move the fish towards its neighbors (unchanged)
 def move_towards_neighbors(fish, neighbors):
     for neighbor in neighbors:
         if calculate_individual_fitness(neighbor, 0) > calculate_individual_fitness(fish, 0):
             fish += step_size * (neighbor - fish)
+
 
 # Move the fish to a new position (unchanged)
 def move_to_new_position(fish):
@@ -71,6 +80,7 @@ def move_to_new_position(fish):
         else:
             fish[(i * 3) + 1] = np.random.choice(album_price)
         fish[(i * 3) + 2] = np.random.choice(user_money_rates[:, 1])
+
 
 # Main loop
 fish_population = initialize_fish_population()
